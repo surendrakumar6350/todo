76c35b76caddd110e6b1a31304e221080d4050bb
surendrakumar6350 <kamleshksks456@gmail.com>
2023-12-10 17:08:00 -0800

    icon added

--- app/home/Click.js
+++ app/home/Click.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { finduser } from '@/helper/userprofile/finduser';
 
 const Click = (props) => {
+  const [piclink, setpiclink] = useState()
     const router = useRouter();
     const {uerid} = props
     const clicked = ()=> {
         router.push(`/profile/${uerid}`)
     }
+
+    const fnc = ()=> {
+      (async()=> {
+      const result = await finduser({id: uerid})
+      const user = result.user
+      
+      setpiclink(user.picture || "https://img.freepik.com/free-photo/user-profile-icon-front-side_187299-39596.jpg?size=338&ext=jpg&ga=GA1.1.1803636316.1701216000&semt=ais")
+      })()
+      }
+      useEffect(fnc,[])
+
+
   return (
     <>
-    <button className='homebtn' onClick={clicked}><img className='homepic' src='https://img.freepik.com/free-photo/user-profile-icon-front-side_187299-39596.jpg?size=338&ext=jpg&ga=GA1.1.1803636316.1701216000&semt=ais'></img></button>
+    <button className='homebtn' onClick={clicked}><img className='homepic' src={piclink}></img></button>
     </>
   )
 }
